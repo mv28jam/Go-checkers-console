c94332ba61e6e2b8e48b1c28c4a59f2f4920b4f3
mv28jam <mv28jam@yandex.ru>
2023-09-05 16:04:18 +0300

    print desc

--- cmd/app/run.go
+++ cmd/app/run.go
@@ -18,7 +18,18 @@ func main() {
 
 	var gameBoard board
 
-	gameBoard.cell[0][1] = figure{true, "⛀", 0}
+	for i := 0; i < len(gameBoard.cell); i++ {
+		for j := 0; j < 3; j++ {
+			if (j+i)%2 != 0 {
+				gameBoard.cell[j][i] = figure{true, "⛀", 1}
+			}
+		}
+		for j := 5; j < 8; j++ {
+			if (j+i)%2 != 0 {
+				gameBoard.cell[j][i] = figure{false, "⛂", 1}
+			}
+		}
+	}
 
 	for i := 0; i < len(gameBoard.cell); i++ {
 		for j := 0; j < len(gameBoard.cell); j++ {
